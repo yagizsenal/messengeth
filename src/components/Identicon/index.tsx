4278527868900c1bfa37df93598a764803e39cc9
Yağız Şenal <yagizsenal@gmail.com>
2021-09-25 13:34:51 +0300

    Updates dependencies

--- src/components/Identicon/index.tsx
+++ src/components/Identicon/index.tsx
@@ -1,27 +1,28 @@
-import Jazzicon from 'jazzicon'
-import { useEffect, useRef } from 'react'
+import {useEffect, useRef} from 'react'
 import styled from 'styled-components'
-import { useActiveWeb3React } from '../../hooks/useActiveWeb3React'
+import {useActiveWeb3React} from '../../hooks/useActiveWeb3React'
+
+const jazzicon = require('@metamask/jazzicon')
 
 const StyledIdenticonContainer = styled.div`
     height: 1rem;
     width: 1rem;
     border-radius: 1.125rem;
-    background-color: ${({ theme }) => theme.bg4};
+    background-color: ${({theme}) => theme.bg4};
 `
 
 export default function Identicon(): JSX.Element {
     const ref = useRef<HTMLDivElement>()
 
-    const { account } = useActiveWeb3React()
+    const {account} = useActiveWeb3React()
 
     useEffect(() => {
         if (account && ref.current) {
             ref.current.innerHTML = ''
-            ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)))
+            ref.current.appendChild(jazzicon(16, parseInt(account.slice(2, 10), 16)))
         }
     }, [account])
 
     // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
-    return <StyledIdenticonContainer ref={ref as any} />
+    return <StyledIdenticonContainer ref={ref as any}/>
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,13 +1,5 @@
 /// <reference types="react-scripts" />
 
-declare module 'react-tradingview-widget'
-
-declare module 'jazzicon' {
-    export default function(diameter: number, seed: number): HTMLElement
-}
-
-declare module 'fortmatic'
-
 interface Window {
     ethereum?: {
         isMetaMask?: true
@@ -20,10 +12,12 @@ interface Window {
 
 declare module 'content-hash' {
     declare function decode(x: string): string
+
     declare function getCodec(x: string): string
 }
 
 declare module 'multihashes' {
     declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
+
     declare function toB58String(hash: Uint8Array): string
 }
